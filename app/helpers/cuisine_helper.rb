95692b14a3ff39b7110be16930f5bb74b9b36ce7
Tomer Desser <tomer.desser@wework.com>
2017-09-25 16:50:34 +0300

    remove redundant files

--- app/helpers/cuisine_helper.rb
+++
@@ -1,2 +0,0 @@
-module CuisineHelper
-end
--- test/controllers/cuisine_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CuisineControllerTest < ActionDispatch::IntegrationTest
-  test "should get all" do
-    get cuisine_all_url
-    assert_response :success
-  end
-
-end
