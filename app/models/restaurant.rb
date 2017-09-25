a55640d1d7ecb8f39e29a7ad80d62fc42abefdc6
Tomer Desser <tomer.desser@wework.com>
2017-09-25 16:50:54 +0300

    remove comment

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,5 @@
-#create_table "restaurants", force: :cascade do |t|
-#  t.string "name"
-#  t.integer "rating"
-#  t.string "cuisine"
-#  t.integer "delivery_sla_in_minutes"
-#  t.datetime "created_at", null: false
-#  t.datetime "updated_at", null: false
-
 class Restaurant < ApplicationRecord
+  belongs_to :cuisine
+  has_many :reviews
+  has_one :restaurant_reviews_metadatum
 end
